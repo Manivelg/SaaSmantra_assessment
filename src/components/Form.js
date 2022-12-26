df2f02cec19ece43960c77ba19f32a6c362e3ba2
Manivelg <121143263+Manivelg@users.noreply.github.com>
2022-12-26 08:21:12 +0530

    Add files via upload

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useFormik } from 'formik'; 
+import * as yup from 'yup';   
+import 'yup-phone';
 
 function Form() {
  
+  const formik = useFormik({
+    initialValues : {
+      Name: '',
+      Email: '',
+      Designation: '',
+      Contact:''
+    },
+    validationSchema:yup.object({
+      Name:yup
+      .string()
+      .min(6, '* Minimum 6 Characters allowed.')
+      .max(15, '* Maximum 15 Characters allowed.')
+      .required('* Enter the valid Name'),
+
+      Email:yup
+      .string()
+      .email('* Invalid E-mail ID')
+      .required('* Enter the E-mail ID'),
+
+      Designation:yup
+       .string()
+       .min(8, '* Minimum 8 Characters allowed.')
+       .max(15, '* Maximum 15 Characters allowed.')
+       .required('* Enter your Designation'),
+      
+      Contact:yup
+       .string()
+       .phone(null, true, 'Invalid mobile Number')
+       .required('* Enter your mobile number.')
+    }),
+    onSubmit: (userInputData) => {
+      console.log(userInputData);
+    }
+  })
 
   return (
     <section id="form">
@@ -9,6 +46,7 @@ function Form() {
         <p className='form_header text-center'><span className='form_line'>Lorem Ipsum</span></p>
       </div>
 
+    <form onSubmit = {formik.handleSubmit} autoComplete="off">
       <div className='form_action'>
         <div className='form_first'>
         <div className='form_second'></div>
@@ -18,8 +56,16 @@ function Form() {
             type="text"
             placeholder='Name'
             className='input_cloud'
+            name="Name"
+            value={formik.values.Name}
+            onChange={formik.handleChange}
            />
-           <p className='error'></p>
+           <p className='error'>
+           {formik.errors.Name ?
+           <span className='errors'>{formik.errors.Name}</span>
+           : null
+           }
+           </p>
         </div>
 
         <div className='first_form'>
@@ -28,8 +74,16 @@ function Form() {
             type="text"
             placeholder='Email'
             className='input_cloud'
+            name="Email"
+            value={formik.values.Email}
+            onChange={formik.handleChange}
            />
-           <p className='error'></p>
+           <p className='error'>
+           {formik.errors.Email ?
+           <span className='errors'>{formik.errors.Email}</span>
+           :null
+            }
+            </p>
         </div>
         </div>
 
@@ -40,8 +94,15 @@ function Form() {
             type="text"
             placeholder='Designation'
             className='input_cloud'
+            name="Designation"
+            value={formik.values.Designation}
+            onChange={formik.handleChange}
            />
-           <p className='error'></p>
+           <p className='error'>
+            {formik.errors.Designation ?
+            <span className='errors'>{formik.errors.Designation}</span>
+            :null}
+            </p>
         </div>
 
         <div className='first_form'>
@@ -50,8 +111,15 @@ function Form() {
             type="text"
             placeholder='Contact'
             className='input_cloud'
+            name="Contact"
+            value={formik.values.Contact}
+            onChange={formik.handleChange}
            />
-           <p className='error'></p>
+           <p className='error'>
+            {formik.errors.Contact ?
+            <span className='error'>{formik.errors.Contact}</span>
+            :null}
+           </p>
         </div>
         </div>
         
@@ -60,7 +128,7 @@ function Form() {
         </div>
 
       </div>
-
+      </form>
     </section>
   )
 }
